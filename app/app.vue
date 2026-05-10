6538042896241767c28c5920d21aefd11e4b2a56
Hyunseok Byun <gumyoincirno@gmail.com>
2026-05-10 12:27:58 +0900

    Initialize project with env usage - 프로젝트 세팅 (그냥 평범한 CLI) - ENV 세팅   - 보아하니 public 은 Client사이드에서 바로 consolelog가 찍힘   - 근데 나머지는 안찍히고 에러 띄우는거보니 알아서 막아주는듯   - 또한 nuxt.config.ts에 runtimConfig으로 주입해줘야 사용가능해보임(아니면 안뜸)

---
+++ app/app.vue
@@ -0,0 +1,8 @@
+<template></template>
+
+<script setup lang="ts">
+console.log("Hello World");
+const runningConfig = useRuntimeConfig();
+
+console.log(runningConfig.public.appName);
+</script>
---
+++ nuxt.config.ts
@@ -0,0 +1,23 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  runtimeConfig: {
+    api: {
+      secret: process.env.NUXT_API_SECRET || 'SECRET NOT FOUNDED',
+    },
+    public: {
+      appName: process.env.NUXT_PUBLIC_APP_NAME || 'APP NOT FOUNDED',
+    }
+  },
+
+  modules: [
+    '@nuxt/a11y',
+    '@nuxt/image',
+    '@nuxt/ui',
+    '@atoms-studio/nuxt-swiftsearch',
+    '@bubblesortt/nuxt-es-toolkit',
+    '@comark/nuxt',
+    '@dargmuesli/nuxt-cookie-control'
+  ]
+})
